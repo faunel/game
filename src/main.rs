d36d6707d67588464c5819f99229f818648388e9
Nilolay <m.alexandrenko@brain.com.ua>
2023-05-29 19:49:35 +0300

    variant1

--- src/main.rs
+++ src/main.rs
@@ -8,45 +8,58 @@ struct Game {
     computer: Choice
 }
 
+struct Result {
+    choice: Choice,
+    name: String
+}
+
+#[derive(Debug)]
 enum Choice {
-    Rock(String),
-    Paper(String),
-    Scissors(String),
+    Rock,
+    Paper,
+    Scissors,
 }
 
 #[derive(PartialEq, Debug)]
 enum Outcome {
-    Win(String, String),
-    Lose(String, String),
-    Draw(String, String),
+    Win,
+    Lose,
+    Draw,
 }
 
 impl Game {
     fn new (player: Choice, computer: Choice) -> Game {
-        Game {player, computer}
+        Game {player,  computer}
     }
 
-    fn compare(self) -> Outcome {
-        match (self.player, self.computer) {
-            (Choice::Rock(p), Choice::Scissors(c)) => Outcome::Win(p, c),
-            (Choice::Rock(p), Choice::Paper(c)) => Outcome::Lose(p, c),
-            (Choice::Paper(p), Choice::Rock(c)) => Outcome::Win(p, c),
-            (Choice::Paper(p), Choice::Scissors(c)) => Outcome::Lose(p, c),
-            (Choice::Scissors(p), Choice::Paper(c)) => Outcome::Win(p, c),
-            (Choice::Scissors(p), Choice::Rock(c)) => Outcome::Lose(p, c),
-            (Choice::Rock(p), Choice::Rock(c)) => Outcome::Draw(p, c),
-            (Choice::Paper(p), Choice::Paper(c)) => Outcome::Draw(p, c),
-            (Choice::Scissors(p), Choice::Scissors(c)) => Outcome::Draw(p, c),
+    fn compare(&self) -> Outcome {
+        match (&self.player, &self.computer) {
+            (Choice::Rock, Choice::Scissors) => Outcome::Win,
+            (Choice::Rock, Choice::Paper) => Outcome::Lose,
+            (Choice::Paper, Choice::Rock) => Outcome::Win,
+            (Choice::Paper, Choice::Scissors) => Outcome::Lose,
+            (Choice::Scissors, Choice::Paper) => Outcome::Win,
+            (Choice::Scissors, Choice::Rock) => Outcome::Lose,
+            _ => Outcome::Draw,
         }
     }
 }
 
 impl Choice {
-    fn get_choice(num: u8) -> Option<Choice> {
+    fn get_choice(num: u8) -> Option<Result> {
         match num {
-            1 => Some(Choice::Rock("Камінь".to_string())),
-            2 => Some(Choice::Scissors("Ножиці".to_string())),
-            3 => Some(Choice::Paper("Папір".to_string())),
+            1 => Some(Result{ 
+                choice: Choice::Rock, 
+                name: "Камінь".to_string()
+            }),
+            2 => Some(Result { 
+                choice: Choice::Scissors, 
+                name: "Ножиці".to_string()
+            }),
+            3 => Some(Result { 
+                choice: Choice::Paper, 
+                name: "Папір".to_string()
+            }),
             _ => None
         }
     }
@@ -90,12 +103,12 @@ fn main() {
                     }
                 };
 
-                let game = Game::new(player, computer);
+                let game = Game::new(player.choice, computer.choice);
 
                 match game.compare() {
-                    Outcome::Win(p, c) => println!("{}. Ви: {}, Комп'ютер: {}", "Ви виграли".green().bold(), p, c),
-                    Outcome::Lose(p, c) => println!("{}. Ви: {}, Комп'ютер: {}", "Ви програли".red().bold(), p, c),
-                    Outcome::Draw(p, c) => println!("{}. Ви: {}, Комп'ютер: {}", "Нічия".yellow().bold(), p, c),
+                    Outcome::Win => println!("{}. Ви: {}, Комп'ютер: {}", "Ви виграли".green().bold(), player.name, computer.name),
+                    Outcome::Lose => println!("{}. Ви: {}, Комп'ютер: {}", "Ви програли".red().bold(), player.name, computer.name),
+                    Outcome::Draw => println!("{}. Ви: {}, Комп'ютер: {}", "Нічия".yellow().bold(), player.name, computer.name),
                 }
             },
             Err(e) => {
@@ -112,84 +125,45 @@ mod tests {
 
     #[test]
     fn test_win_scissors_paper() {
-        assert_eq!(
-            Game::new(Choice::Scissors(String::new()), Choice::Paper(String::new()))
-            .compare(), 
-            Outcome::Win(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Scissors, Choice::Paper).compare(), Outcome::Win);
     }
 
     #[test]
     fn test_win_paper_rock() {
-        assert_eq!(
-            Game::new(Choice::Paper(String::new()), Choice::Rock(String::new()))
-            .compare(), 
-            Outcome::Win(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Paper, Choice::Rock).compare(), Outcome::Win);
     }
 
     #[test]
     fn test_win_rock_scissors() {
-        assert_eq!(
-            Game::new(Choice::Rock(String::new()), Choice::Scissors(String::new()))
-            .compare(), 
-            Outcome::Win(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Rock, Choice::Scissors).compare(), Outcome::Win);
     }
-
 
     #[test]
     fn test_lose_paper_scissors() {
-        assert_eq!(
-            Game::new(Choice::Paper(String::new()), Choice::Scissors(String::new()))
-            .compare(), 
-            Outcome::Lose(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Paper, Choice::Scissors).compare(), Outcome::Lose);
     }
 
     #[test]
     fn test_lose_scissors_rock() {
-        assert_eq!(
-            Game::new(Choice::Scissors(String::new()), Choice::Rock(String::new()))
-            .compare(), 
-            Outcome::Lose(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Scissors, Choice::Rock).compare(), Outcome::Lose);
     }
 
     #[test]
     fn test_lose_rock_paper() {
-        assert_eq!(
-            Game::new(Choice::Rock(String::new()), Choice::Paper(String::new()))
-            .compare(), 
-            Outcome::Lose(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Rock, Choice::Paper).compare(), Outcome::Lose);
     }
 
     #[test]
     fn test_draw_scissors_scissors() {
-        assert_eq!(
-            Game::new(Choice::Scissors(String::new()), Choice::Scissors(String::new()))
-            .compare(), 
-            Outcome::Draw(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Scissors, Choice::Scissors).compare(), Outcome::Draw);
     }
 
     #[test]
-    fn test_draw_paper_paper() {
-        assert_eq!(
-            Game::new(Choice::Paper(String::new()), Choice::Paper(String::new()))
-            .compare(), 
-            Outcome::Draw(String::new(), String::new())
-        );
+    fn test_draw_paper_paper() {assert_eq!(Game::new(Choice::Paper, Choice::Paper).compare(), Outcome::Draw);
     }
 
     #[test]
     fn test_draw_rock_rock() {
-        assert_eq!(
-            Game::new(Choice::Rock(String::new()), Choice::Rock(String::new()))
-            .compare(), 
-            Outcome::Draw(String::new(), String::new())
-        );
+        assert_eq!(Game::new(Choice::Rock, Choice::Rock).compare(), Outcome::Draw);
     }
- 
 }
